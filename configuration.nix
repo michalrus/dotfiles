8335e8c026a5305f67b91a2e916ad3c716f67de0
Michal Rus <m@michalrus.com>
2015-12-25 21:56:55 +0100

    Use GNOME as a desktop manager

--- configuration.nix
+++ configuration.nix
@@ -50,13 +50,16 @@
   # services.printing.enable = true;
 
   # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-  # services.xserver.layout = "us";
+  services.xserver.enable = true;
+  services.xserver.layout = "us";
   # services.xserver.xkbOptions = "eurosign:e";
 
   # Enable the KDE Desktop Environment.
   # services.xserver.displayManager.kdm.enable = true;
   # services.xserver.desktopManager.kde4.enable = true;
+
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome3.enable = true;
 
   users.defaultUserShell = "/run/current-system/sw/bin/zsh";
 
