a554e358d16a749349a712028667279214e30008
Michal Rus <m@michalrus.com>
2015-12-25 22:58:44 +0100

    Remove comments; add firewall

--- configuration.nix
+++ configuration.nix
@@ -1,34 +1,30 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
 
-  # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
-  # Define on which hard drive you want to install Grub.
   boot.loader.grub.device = "/dev/sda";
-
   boot.initrd.luks.devices  =  [ { name = "crypt"; device = "/dev/sda2"; } ];
 
-  networking.hostName = "nixos"; # Define your hostname.
-  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "nixos";
+  networking.nameservers = [ "8.8.8.8" "8.8.4.4" ];
+  networking.firewall.enable = true;
+  networking.firewall.allowPing = true;
+  networking.firewall.allowedTCPPorts = [ ];
+  #networking.wireless.enable = true;       # wireless support via wpa_supplicant
+  networking.networkmanager.enable = true;  # wireless support via NetworkManager
 
-  # Select internationalisation properties.
   i18n = {
     consoleFont = "Lat2-Terminus16";
     consoleKeyMap = "us";
     defaultLocale = "en_US.UTF-8";
   };
 
-  # Set your time zone.
   time.timeZone = "Europe/Warsaw";
 
   # List packages installed in system profile. To search by name, run:
@@ -42,29 +38,16 @@
   programs.zsh.enable = true;
   programs.bash.enableCompletion = true;
 
-  # List services that you want to enable:
+  services.printing.enable = false;
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable the X11 windowing system.
   services.xserver.enable = true;
   services.xserver.layout = "us";
   # services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.kdm.enable = true;
-  # services.xserver.desktopManager.kde4.enable = true;
-
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome3.enable = true;
 
   users.defaultUserShell = "/run/current-system/sw/bin/zsh";
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.extraUsers.m = {
     isNormalUser = true;
     uid = 1000;
