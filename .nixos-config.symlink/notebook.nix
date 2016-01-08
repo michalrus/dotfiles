d143dd63f5ae029256ef5db23851935fc19d4155
Michal Rus <m@michalrus.com>
2016-01-09 00:46:20 +0100

    Add arandr

--- .nixos-config.symlink/notebook.nix
+++ .nixos-config.symlink/notebook.nix
@@ -62,6 +62,7 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
+    arandr
     bc
     cool-retro-term
     dmenu
