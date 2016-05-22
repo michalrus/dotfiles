8c9b2b43fd7c753abd5fe53cd789cb1350967602
Michal Rus <m@michalrus.com>
2016-05-23 00:42:06 +0200

    Make lockX11Displays more robust

--- .nixos-config.symlink/modules/lock-x11-displays.nix
+++ .nixos-config.symlink/modules/lock-x11-displays.nix
@@ -22,14 +22,12 @@ in
       description = "Lock all X11 displays using i3lock (not showing notifications)";
       # This can’t be done in powerManagement.powerDownCommands, because
       # its systemd unit is of the “oneshot” type; we need “forking”.
-      # Besides, this service can be used in WM config — DRY!
+      # Besides, this service should be used in your WM config — DRY!
       serviceConfig.Type = "forking";
       wantedBy = [ "sleep.target" ];
       before = [ "sleep.target" ];
       path = with pkgs; [ procps bash i3lock ];
       script = ''
-        # I need ${pkgs.sudo}. But this still doesn’t guarantee that
-        # /var/setuid-wrappers/sudo will exist… :-)
         pgrep -f xsession | while read p ; do
           printf '%s %s\n' \
             $(cat /proc/$p/environ | tr '\0' '\n' | grep ^DISPLAY | cut -d = -f 2) \
@@ -37,7 +35,7 @@ in
         done | sort | uniq | while read DISPLAY USER ; do
           [ -z "$(grep "^$USER:" /etc/shadow | cut -d : -f 2)" ] && continue # if password empty
           export DISPLAY
-          /var/setuid-wrappers/sudo --background -u $USER bash -c \
+          ${config.security.wrapperDir}/sudo --background -u $USER bash -c \
             'pkill -u $USER -USR1 dunst
              i3lock -n -c 000000 || true
              pkill -u $USER -USR2 dunst'
@@ -45,9 +43,12 @@ in
         '';
     };
 
-    security.sudo.extraConfig = ''
-      %users      ALL=(ALL:ALL) NOPASSWD: ${pkgs.systemd}/bin/systemctl start lock-x11-displays
-      '';
+    security.sudo = {
+      enable = true;
+      extraConfig = ''
+        %users      ALL=(ALL:ALL) NOPASSWD: ${pkgs.systemd}/bin/systemctl start lock-x11-displays
+        '';
+    };
 
   };
 }
