3311ae2c6cd18540fdbc8bc10a24b3984eff8aef
Michal Rus <m@michalrus.com>
2016-06-12 03:23:40 +0200

    Use fetchFromGitHub where appropriate

--- .nixos-config.symlink/pkgs/awf-gtk.nix
+++ .nixos-config.symlink/pkgs/awf-gtk.nix
@@ -7,8 +7,9 @@ super.stdenv.mkDerivation {
     sed 's,/bin/bash,${super.bash}/bin/bash,' -i ./autogen.sh
     ./autogen.sh
     '';
-  src = super.fetchgit {
-    url = https://github.com/valr/awf.git;
+  src = super.fetchFromGitHub {
+    owner = "valr";
+    repo = "awf";
     rev = "191e4283e29845191d46a5eb38baa7b31bbbe8f7";
     sha256 = "0d3hb5gx5ijivzd0d3ldjwzv5sy1cnafvhi534g0wyci62505h0b";
   };
--- .nixos-config.symlink/pkgs/mtr.nix
+++ .nixos-config.symlink/pkgs/mtr.nix
@@ -1,8 +1,9 @@
 super: self:
 
 super.mtr.overrideDerivation (oldAttrs: {
-  src = super.fetchgit {
-    url = https://github.com/traviscross/mtr.git;
+  src = super.fetchFromGitHub {
+    owner = "traviscross";
+    repo = "mtr";
     rev = "faa1bd87e4325b604223aaa8ad5517872ccb7336";
     sha256 = "13sgv38zljybv3fz5s595wgw3bdcb402alalf7j9xlf14ib0027p";
   };
