1849dca8dfe204d370794625e40aa58d78045e9c
Michal Rus <m@michalrus.com>
2016-06-14 18:23:58 +0200

    Use GTK LAF in VisualVM

--- .nixos-config.symlink/pkgs/visualvm.nix
+++ .nixos-config.symlink/pkgs/visualvm.nix
@@ -2,18 +2,24 @@ super: self:
 
 super.stdenv.mkDerivation rec {
   name = "visualvm-1.3.8";
-  src = super.fetchurl {
+  src = super.fetchzip {
     url = "https://java.net/projects/visualvm/downloads/download/release138/visualvm_138.zip";
-    sha256 = "16fqfz0fzshx6hmh55ac4hvggxl646mk4z0d2p8l4ajmavkq3yh5";
+    sha256 = "09wsi85z1g7bwyfhb37vw0gy3wl0j1cy35aj59rg7067q262gy1y";
   };
 
   installPhase = ''
     rm bin/visualvm.exe
-    substituteInPlace etc/visualvm.conf --replace "#visualvm_jdkhome=" "visualvm_jdkhome="
-    substituteInPlace etc/visualvm.conf --replace "/path/to/jdk" "${super.openjdk8}/lib/openjdk/"
-    mkdir $out
-    cp -R . $out/
+
+    substituteInPlace etc/visualvm.conf \
+      --replace "#visualvm_jdkhome=" "visualvm_jdkhome=" \
+      --replace "/path/to/jdk" "${super.openjdk8}/lib/openjdk/" \
+      --replace 'visualvm_default_options="' 'visualvm_default_options="--laf com.sun.java.swing.plaf.gtk.GTKLookAndFeel '
+
+    mkdir $out && cp -R . $out/
+
+    wrapProgram $out/bin/visualvm \
+      --prefix LD_LIBRARY_PATH : "${super.lib.makeLibraryPath [ super.gnome.gtk ]}"
   '';
 
-  buildInputs = [ super.unzip ];
+  buildInputs = [ super.makeWrapper ];
 }
