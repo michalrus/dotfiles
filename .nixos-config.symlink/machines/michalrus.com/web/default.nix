e0c8110cd786cc568e6172f70c55dc39485db833
Michal Rus <m@michalrus.com>
2016-07-06 01:15:58 +0200

    Fix typos in nginx config @ michalrus.com

--- .nixos-config.symlink/machines/michalrus.com/web/default.nix
+++ .nixos-config.symlink/machines/michalrus.com/web/default.nix
@@ -8,7 +8,7 @@ in
 
 {
 
-  networking.firewall.allowedTCPPorts = [ 80 ];
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
 
   systemd.services.nginx.preStart = ''
     mkdir -p "${acmeChallenges}"
@@ -39,7 +39,7 @@ in
         ssl_ciphers 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA';
 
         # DH param generated with `openssl dhparam -out dhparam.pem 4096`.
-        # It’s safe to keep it public, as long as its ≥ 2048 bits,
+        # It’s safe to keep it public, as long as it’s ≥ 2048 bits,
         # cf. http://security.stackexchange.com/a/42418
         ssl_dhparam ${./dhparam.pem};
 
