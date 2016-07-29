7c75966f3fbf9251f33681eb70d8c56f424a49da
Michal Rus <m@michalrus.com>
2016-07-29 20:15:12 +0200

    Clean-up networking.firewall.nonetGroup in every reload of firewall.service

--- .nixos-config.symlink/modules/nonet-group.nix
+++ .nixos-config.symlink/modules/nonet-group.nix
@@ -3,7 +3,10 @@
 with lib;
 
 let
+  groupName = "nonet";
   cfg = config.networking.firewall.nonetGroup;
+  rule  = "OUTPUT -m owner --gid-owner ${groupName} -j REJECT --reject-with icmp-port-unreachable";
+  rule6 = "OUTPUT -m owner --gid-owner ${groupName} -j REJECT --reject-with icmp6-port-unreachable";
 in
 
 {
@@ -16,17 +19,31 @@ in
     };
   };
 
-  config = mkIf cfg.enable {
+  config = mkMerge [
 
-    networking.firewall = {
-      enable = true;
-      extraCommands = ''
-        iptables  -A OUTPUT -m owner --gid-owner nonet -j REJECT --reject-with icmp-port-unreachable
-        ip6tables -A OUTPUT -m owner --gid-owner nonet -j REJECT --reject-with icmp6-port-unreachable
-        '';
-    };
+    {
+      # Clean-up in case someone turns this off. Also, without this,
+      # when reloading the `firewall.service`, the exact same rules
+      # would be added every time.
+      networking.firewall.extraCommands = ''
+        # "${groupName}" group: flush the old rules.
+        iptables  -D ${rule}  2>/dev/null || true
+        ip6tables -D ${rule6} 2>/dev/null || true
+      '';
+    }
 
-    users.extraGroups.nonet = {};
+    (mkIf cfg.enable {
+      networking.firewall = {
+        enable = true;
+        extraCommands = ''
+          # "${groupName}" group: the rules.
+          iptables  -A ${rule}
+          ip6tables -A ${rule6}
+          '';
+      };
 
-  };
+      users.extraGroups."${groupName}" = {};
+    })
+
+  ];
 }
