72b526ed97d32aa758b25e63a278ffc9a59de486
Michal Rus <m@michalrus.com>
2016-08-31 21:57:48 +0200

    Contribute guitarix, bitwig-studio and visualvm to nixpkgs

--- .nixos-config.symlink/pkgs/default.nix
+++ .nixos-config.symlink/pkgs/default.nix
@@ -2,19 +2,16 @@
 
   nixpkgs.config.packageOverrides = super: let self = super.pkgs; in {
     awf-gtk            = (import ./awf-gtk.nix super self);
-    bitwig-studio      = (import ./bitwig-studio super self);
     conkeror-unwrapped = (import ./conkeror super self);
     evince             = (import ./evince.nix super self);
     gettext-emacs      = (import ./gettext-emacs.nix super self);
     gregorio           = (import ./gregorio.nix super self);
-    guitarix           = (import ./guitarix.nix super self);
     influxdb           = (import ./influxdb super self);
     mtr                = (import ./mtr.nix super self);
     mu                 = (import ./mu super self);
     st                 = (import ./st super self);
     tcp-broadcast      = (import ./tcp-broadcast.nix super self);
     transcribe         = (import ./transcribe.nix super self);
-    visualvm           = (import ./visualvm.nix super self);
   };
 
 }
