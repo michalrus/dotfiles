3a959226593d33ee3c50d2b486341229c8d6dab9
Michal Rus <m@michalrus.com>
2017-03-14 23:03:04 +0100

    Update kornel

--- .nixos-config.symlink/machines/michalrus.com/kornel/default.nix
+++ .nixos-config.symlink/machines/michalrus.com/kornel/default.nix
@@ -2,20 +2,6 @@
 
 let
 
-  src = pkgs.fetchFromGitHub {
-    owner = "michalrus";
-    repo = "kornel";
-    rev = "60b1c643ba5eb85f9c98e20973f16bcb4eee7505";
-    sha256 = "0xjsfd9rnrjxax5nm3v5jxz0jd5psdg0jcgnwwkvyfvqg52in2vs";
-  };
-
-  nix-src = pkgs.runCommand "kornel-nix-src" {} ''
-    cp -r ${src} $out
-    cd $out
-    chmod +w .
-    ${pkgs.cabal2nix}/bin/cabal2nix . >kornel.nix
-  '';
-
   compiler = pkgs.unstable-haskell.packages.ghc802.override {
     overrides = self: super: {
       "html-entities" = pkgs.lib.overrideDerivation super.html-entities (oldAttrs: {
@@ -27,7 +13,12 @@ let
     };
   };
 
-  kornel = compiler.callPackage "${nix-src}/kornel.nix" { };
+  kornel = compiler.callCabal2nix "kornel" (pkgs.fetchFromGitHub {
+    owner = "michalrus";
+    repo = "kornel";
+    rev = "277fe37e88ef843ca652de69ec7dc75ce4806999";
+    sha256 = "1xwq2kcxgyqisdbaa6kry41kz030dbq1m9qajf8b7v6jy8m3gk2w";
+  }) {};
 
   user = "kornel";
   dataDir = "/var/lib/${user}";
@@ -55,10 +46,12 @@ in
     '';
     script = ''
       exec kornel-exe \
-        --host irc.freenode.com --port 6697 --ssl \
-        --nick kornel --nickserv-password-file "${dataDir}"/nickserv.pass \
-        --http-snippets-fetch-max $((100 * 1024)) \
+        --host "irc.freenode.com" --port "6697" --ssl \
+        --nick "kornel" --nickserv-password-file "${dataDir}"/nickserv.pass \
+        --http-snippets-fetch-max "$((100 * 1024))" \
         --cleverbot-api-key-file "${dataDir}"/cleverbot.key \
+        --haskell-bot-nicks "lambdabot" \
+        --scala-bot-nicks "multibot_,multibot_1,multibot_2" \
         --channel "#stosowana"
     '';
   };
