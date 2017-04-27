7736b192fdeabee19e6d1e696548a4080dc61fdc
Michal Rus <m@michalrus.com>
2017-04-27 23:16:36 +0200

    Update IntelliJ

--- .nixos-config.symlink/pkgs/idea.nix
+++ .nixos-config.symlink/pkgs/idea.nix
@@ -3,8 +3,8 @@ super: self:
 super.idea // {
   idea-community = super.idea.idea-community.overrideDerivation (oldAttrs: {
     src = super.fetchurl {
-      url = "https://download.jetbrains.com/idea/ideaIC-2017.1-no-jdk.tar.gz";
-      sha256 = "04v5yxg44mvzf348dscjwk0q9jn0gwdjmgivqc3xylpdqsrigyxk";
+      url = "https://download.jetbrains.com/idea/ideaIC-2017.1.2-no-jdk.tar.gz";
+      sha256 = "08r33y32qrfylcvnbxljyjd061gkghm20br7m41xjgmn6y8ry03p";
     };
   });
 }
