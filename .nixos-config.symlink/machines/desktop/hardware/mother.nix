fa7baf01f426b848bbe44cf738b5fbf8f718e392
Michal Rus <m@michalrus.com>
2017-08-15 18:27:43 +0200

    Remove /mnt/Windows from mother.nix

--- .nixos-config.symlink/machines/desktop/hardware/mother.nix
+++ .nixos-config.symlink/machines/desktop/hardware/mother.nix
@@ -26,25 +26,11 @@
     enable = true;
     version = 2;
     device = "/dev/disk/by-id/ata-ST320LT020-9YG142_W044HZ0X";
-#    extraEntries = ''
-#      menuentry "Windows 7 Ultimate" {
-#        insmod part_msdos
-#        insmod ntfs
-#        set root='(hd0,msdos2)'
-#        chainloader +1
-#      }
-#    '';
   };
 
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/a9ad63c9-a869-47da-85cd-0eb10a2eb04f";
     fsType = "ext4";
-  };
-
-  fileSystems."/mnt/Windows" = {
-    device = "/dev/disk/by-uuid/32C0AE4CC0AE165F";
-    fsType = "ntfs";
-    options = [ "fmask=0133" "dmask=0022" ];
   };
 
   swapDevices = [ { device = "/var/swap"; } ];
