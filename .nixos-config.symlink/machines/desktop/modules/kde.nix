77ef3c038145bccfe653ef2bbeb3e13fd1034a1b
Michal Rus <m@michalrus.com>
2017-09-21 19:02:18 +0200

    Run GNOME Keyring in KDE (for Evolution)

--- .nixos-config.symlink/machines/desktop/modules/kde.nix
+++ .nixos-config.symlink/machines/desktop/modules/kde.nix
@@ -12,10 +12,17 @@
   ];
 
   services = {
+    gnome3.gnome-keyring.enable = true;
+
     xserver = {
       displayManager.sddm.enable = true;
       desktopManager.xterm.enable = false;
       desktopManager.plasma5.enable = true;
+
+      # For Evolution password management
+      displayManager.sessionCommands = ''
+        gnome-keyring-daemon || true
+      '';
     };
   };
 
