6bcc9023502a295f127027f07baa141ae1339707
Michal Rus <m@michalrus.com>
2017-10-12 01:43:14 +0200

    Make guest-account.nix work again

--- nixos-config/modules/guest-account.nix
+++ nixos-config/modules/guest-account.nix
@@ -21,7 +21,7 @@ in
         description = ''
           On each login `~guest/` will be deleted and recreated from this directory.
           By default, will result in an empty `~guest/`.
-          '';
+        '';
       };
       home = mkOption {
         type = types.path;
@@ -48,7 +48,7 @@ in
         --usermap='*':guest --groupmap='*':users \
         --exclude=/.Xauthority \
         "${cfg.skeleton}/" "${cfg.home}/"
-      '';
+    '';
 
   in {
 
@@ -61,17 +61,17 @@ in
       extraGroups = cfg.groups;
     };
 
-    services.xserver.displayManager.sessionCommands = ''
+    environment.extraInit = ''
       if [ "$UID" = "${toString cfg.uid}" ] ; then
         ${config.security.wrapperDir}/sudo -n ${cleanUp} || exit 1
       fi
-      '';
+    '';
 
     security.sudo = {
       enable = true;
       extraConfig = ''
         guest ALL = (root) NOPASSWD: ${cleanUp}
-        '';
+      '';
     };
 
   });
