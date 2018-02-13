255fe0e41be90aeb25ad6b2437f3985c0ae70313
Michal Rus <m@michalrus.com>
2018-02-13 01:45:10 +0100

    Update malicious hosts

--- nixos-config/machines/desktop/modules/malicious-hosts.nix
+++ nixos-config/machines/desktop/modules/malicious-hosts.nix
@@ -4,7 +4,7 @@ let
 
   danPollock = pkgs.fetchurl {
     url = "http://someonewhocares.org/hosts/zero/hosts"; # FIXME: not an immutable URL…
-    sha256 = "1dk9514hpsv74dmpvzghh7nrw6z0m0gcsx1f5ki3vc2dyvl8wghd";
+    sha256 = "1csy63vrb6ih65y97c10xyl2skyz8ppql6b9y23xarxwvl7xmr1q";
   };
 
 in
