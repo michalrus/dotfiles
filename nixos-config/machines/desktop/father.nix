edd62018cf8e206241bf449e0cf4af04fc075d7f
Michal Rus <m@michalrus.com>
2018-02-13 01:49:56 +0100

    Update desktop/father.nix

--- nixos-config/machines/desktop/father.nix
+++ nixos-config/machines/desktop/father.nix
@@ -8,6 +8,12 @@
 
   virtualisation.virtualbox.host.enable = true;
 
+  services.tor = {
+    enable = true;
+    client.enable = true;
+    torifiedUsers = [ "robert-tor" ];
+  };
+
   environment.systemPackages = with pkgs; [
     nixos-unstable.ltris
     pcmanfm
@@ -22,6 +28,14 @@
     pdfmod
     perlPackages.PDFAPI2
     fbida
+    di
+    duc
+    libarchive
+    firefox
+    hunspell
+    tor
+    qalculate-gtk
+    afterstep
   ];
 
   users.extraUsers.robert = {
@@ -30,6 +44,14 @@
     description = "Robert Rus";
     extraGroups = [ "wheel" "scanner" "networkmanager" "vboxusers" "cdrom" ];
     dotfiles.profiles = [ "base" "xfce" "git-annex" "robertrus" ];
+  };
+
+  users.extraUsers.robert-tor = {
+    hashedPassword = "$6$rcYySsCDE$X/ilZ3Z4/3dUQ0pPXwnStOQQAsGuoCNY26/29oA4vY6gj.9ZpFYnpaiCUXl4w4sEBdtzqze42LePiIFx51cmM1";
+    isNormalUser = true;
+    description = "Robert Rus (TOR)";
+    extraGroups = [ "scanner" "networkmanager" "cdrom" ];
+    dotfiles.profiles = [ "base" "xfce" "robertrus" ];
   };
 
   hardware.android.automount = let user = config.users.users.robert; in {
