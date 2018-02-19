fc442e2a38a402a0ba9123f85e07180588a155cf
Michal Rus <m@michalrus.com>
2018-02-19 03:54:39 +0100

    Use original pkgs from hie-nix for building HIE

--- nixos-config/overlays/pkgs/haskell-ide-engine.nix
+++ nixos-config/overlays/pkgs/haskell-ide-engine.nix
@@ -5,13 +5,13 @@ let
   hie-nix = super.fetchFromGitHub {
     owner = "domenkozar";
     repo = "hie-nix";
-    rev = "7a8bf3c879e4e3f7b833aacfdef523dd7c92947c";
+    rev = "2b7965f26009b43ecd65c2dcb4d15a53941b726e";
     sha256 = "1b2mv9pvbzk0fy1zjchfmkayya9dg1kq4xk0dqm9bzphz2f4icsv";
   };
 
 in {
 
-  haskell-ide-engine = (import hie-nix { pkgs = super; }).hies.overrideAttrs (oldAttrs: {
+  haskell-ide-engine = (import hie-nix { }).hies.overrideAttrs (oldAttrs: {
     postInstall = "echo ${hie-nix} >$out/prevent-ifd-gc";
   });
 
