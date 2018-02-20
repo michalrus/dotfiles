911d971f8e8326fad20a20fa4949f3d85af6c2da
Michal Rus <m@michalrus.com>
2018-02-20 15:25:43 +0100

    Go back to oldstable kernel in father.nix

--- nixos-config/machines/desktop/hardware/father-acer.nix
+++ nixos-config/machines/desktop/hardware/father-acer.nix
@@ -13,7 +13,6 @@
 
   boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "ehci_pci" "usb_storage" "usbhid" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.kernelModules = [ "kvm-intel" "wl" ];
-  boot.kernelPackages = pkgs.linuxPackages_4_14;
 
   nixpkgs.config.allowUnfree = true; # For the Broadcom driver below...
   boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
