d826ac0ace80fbb9e9ab54162edb6f0f647e71fc
Michal Rus <m@michalrus.com>
2018-03-07 04:17:08 +0100

    Widen GnuCash’s fractions to 1/10⁸

--- nixos-config/overlays/default.nix
+++ nixos-config/overlays/default.nix
@@ -42,11 +42,7 @@ composeOverlays [
       (import ./pkgs/influxdb.nix)
       (import ./pkgs/leksah.nix)
       (import ./pkgs/tcp-broadcast.nix)
-
-      # TODO: move to hledger from this crap
-      (fromNixpkgs "gnucash26" "327a84749ed48a20736fdf20b9dd4f5723b01912" # nixos-unstable
-         "0fgdcy49w073iiy9i65928219n1fy3w61xxsyqn6d8a72dxpcs3n"
-         { permittedInsecurePackages = [ "webkitgtk-2.4.11" ]; })
+      (import ./pkgs/gnucash.nix) # TODO: move to hledger from this crap
 
       # TODO: contribute these:
       (import ./pkgs/gettext-emacs.nix)
