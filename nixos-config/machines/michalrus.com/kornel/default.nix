f5764e612ce7ce15056c6f90c3d2027ae1fc3e44
Michal Rus <m@michalrus.com>
2019-03-03 04:52:00 +0100

    Update kornel

--- nixos-config/machines/michalrus.com/kornel/default.nix
+++ nixos-config/machines/michalrus.com/kornel/default.nix
@@ -5,8 +5,8 @@ let
   src = pkgs.fetchFromGitHub {
     owner = "michalrus";
     repo = "kornel";
-    rev = "a0fdcfb2509af8dc9791bd3695bfcca493012896";
-    sha256 = "19n3qfl5044c8cnwwamd1i8nfqln78sbpf8fnav92irvsi9swmw6";
+    rev = "3aa5bfca8f44b13be1404b333dc0f05dd41483e8";
+    sha256 = "06msh22z3hjqhlyan3kif3yqdhz43bd0bhvi7d5j55z9j10h5jny";
   };
 
   kornel = import src;
@@ -24,10 +24,11 @@ let
     , httpSnippetsFetchMax = Natural/toInteger (100 * 1024)
     , cleverBotApiKey = [ ${dataDir}/cleverbot.key as Text ] : Optional Text
     , smmryApiKey = [ ${dataDir}/smmry.key as Text ] : Optional Text
+    , wolframApiKey = [ ${dataDir}/wolfram.key as Text ] : Optional Text
     , haskellBotNicks = [ "lambdabot" ]
     , scalaBotNicks = [ "multibot", "multibot_", "multibot1", "multibot_1", "multibot2", "multibot_2" ]
-    , channels = [ "#kornel-test" ]
-    , logTraffic = True
+    , channels = [ "#scala.pl", "#stosowana" ]
+    , logTraffic = False
     }
   '';
 
