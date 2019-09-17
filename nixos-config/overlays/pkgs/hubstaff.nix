d86c9e50619fe79859110f8693be050b57c64731
Michal Rus <m@michalrus.com>
2019-09-17 15:35:18 +0200

    Update hubstaff

--- nixos-config/overlays/pkgs/hubstaff.nix
+++ nixos-config/overlays/pkgs/hubstaff.nix
@@ -4,8 +4,8 @@ self: super:
 
   hubstaff = let
 
-    version = "1.4.9-86828309";
-    sha256 = "0p9b7s2damzxmbrm8m97bj06g0faslbjw51dmxq8icz6ldbqsspx";
+    version = "1.4.10-848554d6";
+    sha256 = "1hwncdzpzawrwswr3ibhxny0aa5k9f8f2qf636bdzqilwhv6342z";
 
   in super.nixos-unstable.hubstaff.overrideAttrs (drv: {
     name = "hubstaff-${version}";
