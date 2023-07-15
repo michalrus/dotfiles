a9e2a8d63041370c299697b823e531d7331fd60c
Michal Rus <m@michalrus.com>
2023-07-15 17:32:25 +0200

    Add Ledger Nano S Plus

--- nixos-config/machines/desktop/base-mine.nix
+++ nixos-config/machines/desktop/base-mine.nix
@@ -125,6 +125,13 @@
     # YubiKey
     pkgs.yubikey-personalization
 
+    # Ledger Nano S Plus
+    pkgs.ledger-udev-rules
+    (pkgs.writeTextDir "lib/udev/rules.d/19-ledger-generic.rules" ''
+      # The newest Ledger rule (not yet in Nixpkgs):
+      SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0005|5000|5001|5002|5003|5004|5005|5006|5007|5008|5009|500a|500b|500c|500d|500e|500f|5010|5011|5012|5013|5014|5015|5016|5017|5018|5019|501a|501b|501c|501d|501e|501f", TAG+="uaccess", TAG+="udev-acl"
+    '')
+
   ];
 
   virtualisation.podman.enable = true;
