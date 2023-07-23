70b78068a984400139dc8d6bb4d46b5a3bfa0aac
Michal Rus <m@michalrus.com>
2023-07-23 18:49:08 +0200

    Fix ‘machines/lenovo-x1’

--- machines/lenovo-x1/features/openvpn-michalrus_com.nix
+++ machines/lenovo-x1/features/openvpn-michalrus_com.nix
@@ -27,7 +27,7 @@ in
         persist-key
         persist-tun
         mute-replay-warnings
-        ca ${../../../nixos-config/machines/michalrus.com/openvpn/ca.crt}
+        ca ${../../../machines/michalrus_com/features/openvpn/ca.crt}
         cert ${dataDir}/client.crt
         key ${dataDir}/client.key
         remote-cert-tls server
@@ -39,6 +39,6 @@ in
     };
   };
 
-  networking.extraHosts = (import ../../../nixos-config/machines/michalrus.com/openvpn/common.nix { inherit lib; }).extraHosts;
+  networking.extraHosts = (import ../../../machines/michalrus_com/features/openvpn/common.nix { inherit lib; }).extraHosts;
 
 }
