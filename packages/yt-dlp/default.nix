f6343dd41a43dcaeebe553b3d79dea47931800b0
Michal Rus <m@michalrus.com>
2023-07-29 19:23:52 +0200

    Add shell completions & manual back to ‘yt-dlp’

--- packages/yt-dlp/default.nix
+++ packages/yt-dlp/default.nix
@@ -1,4 +1,4 @@
-{ flake, system, lib, yt-dlp }:
+{ flake, system, lib, yt-dlp, pandoc, which }:
 
 # XXX: it will return a newer one – either from nixpkgs or yt-dlp itself
 # – looking at versions; if versions are equal, it’ll take our input
@@ -18,4 +18,13 @@ let
 in if theyAreNewer then theirs else theirs.overridePythonAttrs (drv: {
   src = ours;
   version = ourVersion + "-nightly-" + lib.substring 0 8 ours.lastModifiedDate;
+  preBuild = (drv.preBuild or "") + ''
+    # Completions and man page are published to pypi, but have to be built for our custom src:
+    patchShebangs devscripts/
+    sed -r 's,/usr/bin/env python3,'"$(${lib.getExe which} python3)"',g' -i Makefile
+    (
+      export PATH="${lib.makeBinPath [ pandoc ]}:$PATH"
+      make completions yt-dlp.1
+    )
+  '';
 })
