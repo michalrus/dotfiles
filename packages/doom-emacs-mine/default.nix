305339f7d4f23323fe22256b8acdd3bbf50b2428
Michal Rus <m@michalrus.com>
2023-08-29 02:44:19 +0200

    Fix a Magit `:pin` bug

--- packages/doom-emacs-mine/default.nix
+++ packages/doom-emacs-mine/default.nix
@@ -4,5 +4,5 @@ doom-emacs.override {
   doomPackagesEl = ./packages.el;
   doomInitEl     = ./init.el;
   doomConfigEl   = ./config.el;
-  vendorHash     = "sha256-dAuhQK5JlHQruqZOyFHsyjGXeslI2dDOYHlfEkfiExY=";
+  vendorHash     = "sha256-GbY7Mput88pvn1/tQjZA4pUsugjBeaLhnY9Pj7yxKKo=";
 }
--- packages/doom-emacs-mine/packages.el
+++ packages/doom-emacs-mine/packages.el
@@ -3,4 +3,9 @@
 (package! git-link)
 (package! gumshoe)
 
+;; Prevent this double-pin Magit bug:
+;;   • <https://github.com/doomemacs/doomemacs/issues/7363#issuecomment-1696530746>
+;;   • <https://github.com/magit/magit/issues/4994>
+(unpin! git-commit)
+
 ;
