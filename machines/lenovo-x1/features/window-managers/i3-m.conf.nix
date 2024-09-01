79609293953b42231996a97aa596a9fa6ea0f0df
Michal Rus <m@michalrus.com>
2024-09-01 10:57:24 +0200

    Tweak autostart

--- machines/lenovo-x1/features/window-managers/i3-m.conf.nix
+++ machines/lenovo-x1/features/window-managers/i3-m.conf.nix
@@ -1,13 +1,11 @@
 ''
 
-assign [class="^Firefox$" instance="^Navigator$"] 2
+assign [class="^Chromium-browser$"] 2
 
 assign [class="^mpv$"] 2
 
-assign [class="^[Qq]emu-"] 4
-assign [class="^VirtualBox$"] 4
-
-assign [class="^Emacs$" title="^org$"] 1
+assign [class="^[Qq]emu-"] 9
+assign [class="^VirtualBox$"] 9
 
 assign [class="^qjackctl$"] 4
 for_window [class="^qjackctl$"] [title="^JACK Audio Connection Kit"] floating enable
