6ed835a44cdee9e961116ac0fb95dba17837ab09
Michal Rus <m@michalrus.com>
2025-12-10 20:17:29 +0100

    Also add `mpvScripts.mpris` to `accuradio`

--- packages/accuradio/default.nix
+++ packages/accuradio/default.nix
@@ -1,8 +1,12 @@
-{ writeShellApplication, coreutils, util-linux, skim, gnused, gnugrep, curl, jq, mpv }:
+{ writeShellApplication, coreutils, util-linux, skim, gnused, gnugrep, curl, jq, mpv, mpvScripts }:
+
+let
+  mpv' = mpv.override { scripts = with mpvScripts; [ mpris ]; };
+in
 
 writeShellApplication {
   name = "accuradio";
-  runtimeInputs = [ coreutils util-linux skim gnused gnugrep curl jq mpv ];
+  runtimeInputs = [ coreutils util-linux skim gnused gnugrep curl jq mpv' ];
   text = builtins.readFile ./accuradio.sh;
   derivationArgs.meta.description = "Plays AccuRadio.com in the terminal";
 }
