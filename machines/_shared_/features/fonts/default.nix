29a118dd126c0506233a5d69e8d8f7e6d4e5bc52
Michal Rus <m@michalrus.com>
2026-01-11 15:32:27 +0100

    Add `google-fonts` but without the Emoji clash

--- machines/_shared_/features/fonts/default.nix
+++ machines/_shared_/features/fonts/default.nix
@@ -1,5 +1,15 @@
 { flake, config, pkgs, ... }:
 
+let
+  google-fonts--no-emoji = pkgs.symlinkJoin {
+    name = "google-fonts--no-emoji";
+    paths = [ pkgs.google-fonts ];
+    postBuild = ''
+      find "$out/share/fonts" -type l -iname '*emoji*' -delete
+    '';
+  };
+in
+
 {
   fonts.enableDefaultPackages = false;  # DejaVu fonts break Noto Sans Emoji fallback
 
@@ -10,7 +20,7 @@
     noto-fonts
     noto-fonts-color-emoji
     noto-fonts-cjk-sans
-    #google-fonts  # They break ‘noto-fonts-emoji’
+    google-fonts--no-emoji  # The original `google-fonts` break ‘noto-fonts-emoji’
     liberation_ttf
   ];
 
