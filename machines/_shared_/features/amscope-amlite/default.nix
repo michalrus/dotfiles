49a91ce2c8eaf1d6af721638749a318f867407ac
Michal Rus <m@michalrus.com>
2026-01-14 18:22:09 +0100

    Add one more AmScope camera to `udev`

--- machines/_shared_/features/amscope-amlite/default.nix
+++ machines/_shared_/features/amscope-amlite/default.nix
@@ -9,6 +9,7 @@
   # FIXME: add a v4l2loopback from libtoupcam.so for other apps – but what about the control knobs?
   # FIXME: not 0666
   services.udev.extraRules = ''
+    SUBSYSTEM=="usb", ATTR{idVendor}=="0547", ATTR{idProduct}=="3018", MODE="0666"
     SUBSYSTEM=="usb", ATTR{idVendor}=="0547", ATTR{idProduct}=="3b14", MODE="0666"
   '';
 }
