a14c4d28fa89978b0d71894a3d7ec682a1bc7eb1
Michal Rus <m@michalrus.com>
2026-01-16 23:23:29 +0100

    Add a personal `macbook` account

--- machines/_shared_/home/identity-personal/default.nix
+++ machines/_shared_/home/identity-personal/default.nix
@@ -49,6 +49,12 @@
       UserKnownHostsFile ~/.ssh/known_hosts.d/home-network
       User root
 
+    Host macbook 10.77.2.99
+      Hostname 10.77.2.99
+      HostKeyAlias macbook
+      UserKnownHostsFile ~/.ssh/known_hosts.d/home-network
+      User m
+
     Host dell-home-server 10.77.2.11
       Hostname 10.77.2.11
       HostKeyAlias dell-home-server
@@ -66,6 +72,9 @@
     aneta ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB1nWQ6Rb+a8oxWf5IgfzSAENh7Kug0QuNRpaZM/47Qf
     aneta ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDoM/52op8WoSO26i2SJEpZVMunHpbCDu/rEMOQ3BW9xBvtQIWb8ViveUsuEhp1e0xYCj5DuMAPixJZPGFQRjEuOKcStyvHW117UsKcuCcsk2qqaEIgt398d31fBQJwBWfAY46wuYkMp2TUUfpkyPRMHjIsRrZrcz5sv0Kh/YmVyiZ9mUOtJx9GBA09uXyDwB0Rh7RK3W3G7CvPnRzPXKGfNzrW06uofpfQF633rxRKi0DpxINTQ8Ft8C4/jg2lt79Ln3D+sxmvKjb8kIKIaLCl7l81OTy/yrVoo1Cv/c39nTjB+A7lzGRWCj671NIUzKXxjsGiCa8HKAbIQ5T/o9aMDQpjtoDHyG5AY0i4VEzsykkF3DawzNXW7p0mZnLMSuiJx5TqvpaJmVcuYi+OXXF8HEAcFwY4dEi2NdKUBayod7YDRU8txHDjmLVE6rNlQFzoP+PnQ+pisoQCQ2kt10XgJju+xaqflJ+SNli9J4rWpCBhowBVcNsZjBokjESCftjRQvYt2trnkW+MIwj0DXg64317gKR2NsK75j41K66BSa7YLfhJSsM0E5f0n3MWU0TRJAiC8+4JvqERifeM8WvnfFb2Jldpzno/1JrBv5Nru06qsQt9zxWW3bKgC2nQ1npJzeR1s5uTGZsFnVVULJicStQXdQo32RSTMTgGsqwH0Q==
     dell-home-server ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHIF7BIoIAPX1gBffDvuKoHHXrHRbgJdDze6DhH97N5B
+    macbook ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILZEeBucsKC/jKItBn8MQxSBSUiO2oMCHn5u5iSxI7Ac
+    macbook ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDBkyRxLLxPV4SY0uvlfyLBHNm9z6yorToW4B6LnqmpVd9xHlRxxq/qT99LozZohvSbKIK7JDEaho/hA4ClecyYCsDjOJumxLHXk3+14bYFPpO1BqE0DNdiFza6zlyaCQsuxwWGoWMNDHwgBgpS7XupSYHT4hGocY3lDL0GXKb372CIsgw+qExSAzZ7agQ4mxSHT/54qnO3BEiIEHAvCHmZh18ozVMNu921FHa6IFBp7H68vzk/hsU7vgx41r/5p9IVHdT6oII7gX0pCYTygllxf3jqkXnUoQW4H3C4WtapXbmdfZNXBnVj1iBTNzg2fMKuqeTYPtrieC4x5fD7BUijUnMTJZ6LiyGN85VDw6ELn6uvwhxy2T6COXp+WlFF45BkHUfikx4AcMz3ah+aZ1B/wkAoWorgTeuWnJjIqHR6z4cYeW88j4cZ9ZMnFRvg4XV638Qr5QaNhejAUr2Xcf0ohmtjNt6ZkZXhncX3JVWepp5RloZ+340kKfRt8GJ8mb8=
+    macbook ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBCsy6/DavcIK0qddj+r5Hp4RA6LAYl8V9MvGLYxhHe63bT4keA+2kK15EQSuM6TxCy+yaHnh424yMdJC6FAiAYg=
     monstrum ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAII+f73fJcgbVVbfMI+lAzOk582TGp6t7O3jFxk60B8
   '';
 }
