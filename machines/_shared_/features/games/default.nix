2bebab685dc7f16fe9a1bfabc52eb6799ca2e6b0
Michal Rus <m@michalrus.com>
2026-03-07 20:44:02 +0100

    chore: update other flake inputs

--- machines/_shared_/features/games/default.nix
+++ machines/_shared_/features/games/default.nix
@@ -1,11 +1,24 @@
-{pkgs, ...}: let
+{
+  pkgs,
+  lib,
+  ...
+}: let
   unfree = import pkgs.path {
     inherit (pkgs.stdenv.hostPlatform) system;
     config.allowUnfree = true;
   };
+  retroarch-full = unfree.retroarch.withCores (
+    cores:
+      lib.filter (
+        c:
+          (c ? libretroCore)
+          && (lib.meta.availableOn pkgs.stdenv.hostPlatform c)
+          && (c.core or "" != "fbalpha2012") # broken on current 25.11
+      ) (lib.attrValues cores)
+  );
 in {
   environment.systemPackages = with pkgs; [
     aisleriot
-    unfree.retroarch-full
+    retroarch-full
   ];
 }
