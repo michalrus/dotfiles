4206000f4cd8b79e0b7b3acdb8a5b253d84b0c7b
Michal Rus <m@michalrus.com>
2026-03-09 18:03:08 +0100

    fix(pass): don’t run the Conventional Commits checks in `pass`

--- machines/_shared_/home/password-store/default.nix
+++ machines/_shared_/home/password-store/default.nix
@@ -5,7 +5,15 @@
 
   programs.password-store = {
     enable = true;
-    package = pkgs.pass;
+    package = pkgs.pass.overrideAttrs (old: {
+      # Don’t run the Conventional Commits checks on `pass` commits:
+      postInstall =
+        (old.postInstall or "")
+        + ''
+          wrapProgram $out/bin/pass --set SKIP_COG 1
+        '';
+      nativeBuildInputs = (old.nativeBuildInputs or []) ++ [pkgs.makeWrapper];
+    });
     settings.PASSWORD_STORE_DIR = "$HOME/.password-store";
   };
 
