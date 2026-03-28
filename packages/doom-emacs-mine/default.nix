dd63bea8f960d29889cbde5ab8287703d610c40e
Michal Rus <m@michalrus.com>
2026-03-28 18:55:54 +0100

    feat(lsp): use LSP within `nix-mode`

--- packages/doom-emacs-mine/default.nix
+++ packages/doom-emacs-mine/default.nix
@@ -5,7 +5,7 @@ doom-emacs.override {
   doomConfigEl = ./config.el;
   vendorHash =
     {
-      "x86_64-linux" = "sha256-uolaZFpEdPRe8kubaD25jf6a/J8hMdts+LqkZUGJSsw=";
+      "x86_64-linux" = "sha256-ymKiTjEe8BQQs6W+aU6ZkpFFahdCV5r5i1T3atXYjUs=";
       "aarch64-darwin" = ""; # FIXME
     }.${
       doom-emacs.system
